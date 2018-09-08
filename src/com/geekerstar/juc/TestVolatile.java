13457dca4f93e1bbbbd985d98653fcd4447a4540
geekerstar <247507792@qq.com>
2018-09-08 14:31:21 +0800

    init

---
+++ src/com/geekerstar/juc/TestVolatile.java
@@ -0,0 +1,54 @@
+package com.geekerstar.juc;
+
+/**
+ * @Author: Geekerstar(QQ : 247507792)
+ * @Date: 2018/9/8 13:07
+ * @Description:
+ * 一、volatile 关键字：当多个线程进行共享操作时，可以保证内存中的数据是可见的。
+ *                      相较于synchronized是一种较为轻量级的同步策略
+ *
+ * 注意：
+ * 1、volatile 不再具备“互斥性”
+ * 2、volatile 不能保证变量的“原子性”
+ *
+ */
+
+
+
+public class TestVolatile {
+    public static void main(String[] args) {
+        ThreadDemo td = new ThreadDemo();
+        new Thread(td).start();
+
+        while(true){
+            if(td.isFlag()){
+                System.out.println("--------------");
+                break;
+            }
+        }
+    }
+}
+
+class ThreadDemo implements Runnable{
+    private volatile boolean flag = false;
+
+    @Override
+    public void run(){
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        flag = true;
+
+        System.out.println("flag="+isFlag());
+    }
+
+    public boolean isFlag(){
+        return flag;
+    }
+
+    public void setFlag(boolean flag){
+        this.flag = flag;
+    }
+}
